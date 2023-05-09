e631d89e8468b07044c4024423aba4ebcc0796d0
Andres castañeda <estiven.casta12@gmail.com>
2023-05-09 09:17:38 -0500

    se agrega la informacion a los contactos anteriores y se añade nuevo contacto 4

--- script.js
+++ script.js
@@ -8,15 +8,18 @@ function Contacts(id, name, lastname, phone, address) {
 }
 
 let contacts = [
-  new Contacts("JUAN", "RODRIGUEZ"),
-  new Contacts("jHON", "CARMELO"),
-  new Contacts("CAMILO", "CIFUENTEZ"),
+  new Contacts(1, "JUAN", "RODRIGUEZ", 3225424074, "cra 40 #6-61, Envigado"),
+  new Contacts(2, "jHON", "CARMELO", 3054895684, "clle 2b 61, itagui"),
+  new Contacts(3, "CAMILO", "CIFUENTEZ", 3254584269, "cra 80 #2-69, medellin"),
 ];
 
 function addContact(contact = new Contacts()) {
   contacts.push(contact);
   console.log(contacts);
 }
+
+addContact(new Contacts(4, "Chris", "Castaño", 3015089658, "Calle 4B #101-58, Medellin"));
+  
 
 function deleteContact(id) {
   contacts.splice(id - 1, 1);
