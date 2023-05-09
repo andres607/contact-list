af6017e7d405c9675dfb8cdf967db3dce3b08078
Andres castañeda <estiven.casta12@gmail.com>
2023-05-09 07:59:17 -0500

    se agrega la lista predefinida

---
+++ script.js
@@ -0,0 +1,11 @@
+function Contacts(name, lastname,) {
+  this.name = name;
+  this.lastname = lastname;
+}
+
+let contacts = [
+  new Contacts("JUAN", "RODRIGUEZ"),
+  new Contacts("jHON", "CARMELO"),
+  new Contacts("CAMILO", "CIFUENTEZ"),
+];
+
